4215e5dc2f906aaeecd458373d6a2cceb6d01b18
xIvan0ff <hrisivanov2@gmail.com>
2021-12-06 15:15:58 +0200

    update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-if (isset($_GET['file'])) {
+$domain = $_SERVER['SERVER_NAME'];
+if (isset($_GET['file']) or substr($domain, 0, 6) === "google") {
 	echo (file_get_contents("randomstringbroidk/index_real.html"));
 } else {
 	echo (file_get_contents("randomstringbroidk/index_illusion.html"));
